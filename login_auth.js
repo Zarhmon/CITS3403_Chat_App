1d3c2975f38c6d2983ccb91cf6583cd5d2ad89c1
Zarhmon <zarhmon.k@gmail.com>
2023-05-01 16:41:41 +0800

    Pushing latest

--- login_auth.js
+++ login_auth.js
@@ -1,59 +1,58 @@
 // Google Sign-In API initialization
 function init() {
-    gapi.load('auth2', function () {
-      gapi.auth2.init({
-        client_id: '256996938210-31ionlslvrguote7mhd46if0pukld4ea.apps.googleusercontent.com',
-        cookiepolicy: 'single_host_origin',
-      });
+  gapi.load('auth2', function () {
+    gapi.auth2.init({
+      client_id: '256996938210-31ionlslvrguote7mhd46if0pukld4ea.apps.googleusercontent.com',
+      cookiepolicy: 'single_host_origin',
     });
-  }
-  
+  });
+}
 
- // Google Sign-In API callback function
+
+// Google Sign-In API callback function
 function onSignIn(googleUser) {
-    // Get the user's ID token, used to authenticate backend server
-    var id_token = googleUser.getAuthResponse().id_token;
-  
-    // Get the user's basic profile information
-    var profile = googleUser.getBasicProfile();
-  
-    // Store the user's information in sessionStorage
-    sessionStorage.setItem("user_name", profile.getName());
-    sessionStorage.setItem("user_image", profile.getImageUrl());
-  
-    // Redirect the user to the chat application
-    window.location.href = "main.html";
-  }
-  
-  
-  function signOut() {
-    if (sessionStorage.getItem("user_name") === "Guest") {
-      // If the user is a guest, clear the sessionStorage and redirect to login page
+  // Get the user's ID token, used to authenticate backend server
+  var id_token = googleUser.getAuthResponse().id_token;
+
+  // Get the user's basic profile information
+  var profile = googleUser.getBasicProfile();
+
+  // Store the user's information in sessionStorage
+  sessionStorage.setItem("user_name", profile.getName());
+  sessionStorage.setItem("user_image", profile.getImageUrl());
+
+  // Redirect the user to the chat application
+  window.location.href = "main.html";
+}
+
+
+function signOut() {
+  if (sessionStorage.getItem("user_name") === "Guest") {
+    // If the user is a guest, clear the sessionStorage and redirect to login page
+    sessionStorage.clear();
+    window.location.href = "login.html";
+  } else {
+    // For Google accounts, use the Google API to sign out
+    const auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(() => {
       sessionStorage.clear();
       window.location.href = "login.html";
-    } else {
-      // For Google accounts, use the Google API to sign out
-      const auth2 = gapi.auth2.getAuthInstance();
-      auth2.signOut().then(() => {
-        sessionStorage.clear();
-        window.location.href = "login.html";
-      });
-    }
+    });
   }
-  
+}
+
 // Add a guest sign-in function
 function guestSignIn(event) {
-    // Prevent any default behavior
-    if (event) event.preventDefault();
-  
-    // Store guest user information in sessionStorage
-    sessionStorage.setItem("user_name", "Guest");
-    sessionStorage.setItem("user_image", "https://via.placeholder.com/40");
-  
-    // Redirect the user to the chat application
-    window.location.href = "main.html";
-  }
-  
-  // initialize the Google Sign-In API
-  init();
-  
+  // Prevent any default behavior
+  if (event) event.preventDefault();
+
+  // Store guest user information in sessionStorage
+  sessionStorage.setItem("user_name", "Guest");
+  sessionStorage.setItem("user_image", "https://via.placeholder.com/40");
+
+  // Redirect the user to the chat application
+  window.location.href = "main.html";
+}
+
+// initialize the Google Sign-In API
+init();
--- main_script.js
+++ main_script.js
@@ -17,10 +17,10 @@ if (
 }
 // select the chat input field and send button
 const inputField = document.querySelector(
-".chat-input input[type='text']"
+  ".chat-input input[type='text']"
 );
 const sendButton = document.querySelector(
-".chat-input input[type='submit']"
+  ".chat-input input[type='submit']"
 );
 
 // select the chat box where messages will be displayed
@@ -28,31 +28,31 @@ const chatBox = document.querySelector(".chat-box");
 
 // function to add a new message to the chat box
 function addMessage(messageText, isOutgoing) {
-// create a new message div
-const messageDiv = document.createElement("div");
-messageDiv.classList.add("message");
+  // create a new message div
+  const messageDiv = document.createElement("div");
+  messageDiv.classList.add("message");
 
-// add an avatar image to the message div
-const avatarDiv = document.createElement("div");
-avatarDiv.classList.add("avatar");
-messageDiv.appendChild(avatarDiv);
+  // add an avatar image to the message div
+  const avatarDiv = document.createElement("div");
+  avatarDiv.classList.add("avatar");
+  messageDiv.appendChild(avatarDiv);
 
-// add the message text to the message div
-const textDiv = document.createElement("div");
-textDiv.classList.add("text");
-textDiv.innerHTML = "<p>" + messageText + "</p>";
-messageDiv.appendChild(textDiv);
+  // add the message text to the message div
+  const textDiv = document.createElement("div");
+  textDiv.classList.add("text");
+  textDiv.innerHTML = "<p>" + messageText + "</p>";
+  messageDiv.appendChild(textDiv);
 
-// add a class to the message div to indicate whether it's an outgoing message or not
-if (isOutgoing) {
-messageDiv.classList.add("outgoing");
-}
+  // add a class to the message div to indicate whether it's an outgoing message or not
+  if (isOutgoing) {
+    messageDiv.classList.add("outgoing");
+  }
 
-// add the message div to the chat box
-chatBox.appendChild(messageDiv);
+  // add the message div to the chat box
+  chatBox.appendChild(messageDiv);
 
-// scroll the chat box to the bottom to show the latest message
-chatBox.scrollTop = chatBox.scrollHeight;
+  // scroll the chat box to the bottom to show the latest message
+  chatBox.scrollTop = chatBox.scrollHeight;
 }
 
 // function to handle sending a new message
@@ -66,7 +66,7 @@ function sendMessage() {
     inputField.value = "";
     addMessage(messageText, true);
     interpret(messageText);
-    
+
   }
 }
 
@@ -75,9 +75,9 @@ sendButton.addEventListener("click", sendMessage);
 
 // add an event listener to the input field to handle sending a new message when the user presses Enter
 inputField.addEventListener("keydown", function (event) {
-if (event.key === "Enter") {
-sendMessage();
-}
+  if (event.key === "Enter") {
+    sendMessage();
+  }
 });
 
 function interpret(text) {
