9b0b90a292256c0040d6d60d8d018537b2ba03af
Zarhmon <zarhmon.k@gmail.com>
2023-05-08 13:37:28 +0800

    starting flask

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+# MySQL configurations
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'your_mysql_user'
+app.config['MYSQL_PASSWORD'] = 'your_mysql_password'
+app.config['MYSQL_DB'] = 'your_database_name'
+
+mysql = MySQL(app)
+
+# Secret key for flash messages
+app.secret_key = 'your_secret_key'
