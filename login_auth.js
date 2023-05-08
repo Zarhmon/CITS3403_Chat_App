2f9e304219c109c4c817ec1f90d3a753dafa96f7
Zarhmon <zarhmon.k@gmail.com>
2023-05-08 13:29:09 +0800

    removed google auth, added sign up form

--- login_auth.js
+++ login_auth.js
@@ -1,47 +1,4 @@
-// Google Sign-In API initialization
-function init() {
-  gapi.load('auth2', function () {
-    gapi.auth2.init({
-      client_id: '256996938210-31ionlslvrguote7mhd46if0pukld4ea.apps.googleusercontent.com',
-      cookiepolicy: 'single_host_origin',
-    });
-  });
-}
-
-
-// Google Sign-In API callback function
-function onSignIn(googleUser) {
-  // Get the user's ID token, used to authenticate backend server
-  var id_token = googleUser.getAuthResponse().id_token;
-
-  // Get the user's basic profile information
-  var profile = googleUser.getBasicProfile();
-
-  // Store the user's information in sessionStorage
-  sessionStorage.setItem("user_name", profile.getName());
-  sessionStorage.setItem("user_image", profile.getImageUrl());
-
-  // Redirect the user to the chat application
-  window.location.href = "main.html";
-}
-
-
-function signOut() {
-  if (sessionStorage.getItem("user_name") === "Guest") {
-    // If the user is a guest, clear the sessionStorage and redirect to login page
-    sessionStorage.clear();
-    window.location.href = "login.html";
-  } else {
-    // For Google accounts, use the Google API to sign out
-    const auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(() => {
-      sessionStorage.clear();
-      window.location.href = "login.html";
-    });
-  }
-}
-
-// Add a guest sign-in function
+// Function to handle guest sign-in
 function guestSignIn(event) {
   // Prevent any default behavior
   if (event) event.preventDefault();
@@ -54,5 +11,14 @@ function guestSignIn(event) {
   window.location.href = "main.html";
 }
 
-// initialize the Google Sign-In API
-init();
+// Function to handle sign-out
+function signOut() {
+  if (sessionStorage.getItem("user_name") === "Guest") {
+    // If the user is a guest, clear the sessionStorage and redirect to login page
+    sessionStorage.clear();
+    window.location.href = "login.html";
+  }
+}
+
+// Attach event listener to guest sign-in button
+document.getElementById("guest-signin").addEventListener("click", guestSignIn);
