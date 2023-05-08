732f10a6426fa74027ae196f514def3f6fd26bd0
Zarhmon <zarhmon.k@gmail.com>
2023-05-08 14:20:18 +0800

    added flask and requirments / vm

--- app.py
+++ app.py
@@ -13,3 +13,55 @@ mysql = MySQL(app)
 
 # Secret key for flash messages
 app.secret_key = 'your_secret_key'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user_details = request.form
+        email = user_details['email']
+        password = user_details['password']
+
+        # Check if the user exists in the database
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE email = %s AND password = %s", (email, password))
+        user = cur.fetchone()
+        cur.close()
+
+        if user:
+            flash('Logged in successfully!', 'success')
+            return redirect(url_for('main'))
+        else:
+            flash('Invalid email or password.', 'danger')
+
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        user_details = request.form
+        email = user_details['reg_email']
+        password = user_details['reg_password']
+
+        # Check if the user already exists in the database
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE email = %s", [email])
+        user = cur.fetchone()
+
+        if user:
+            flash('A user with this email already exists.', 'danger')
+        else:
+            # Insert the new user into the database
+            cur.execute("INSERT INTO users(email, password) VALUES(%s, %s)", (email, password))
+            mysql.connection.commit()
+            cur.close()
+            flash('Account created successfully! Please log in.', 'success')
+            return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
