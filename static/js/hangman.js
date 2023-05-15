c568473e1589ef540a31793e655981a099d8cd7f
Farrel-K-UWA <23057954@student.uwa.edu.au>
2023-05-15 16:39:48 +0800

    Update hangman.js and main_script.js
    Replaced 'letter' with <b>letter</b> in some places. Removed consecGuesses and makes difficulty affect only # of attempts

--- static/js/hangman.js
+++ static/js/hangman.js
@@ -10,12 +10,12 @@ var word; // the actual word, e.g. "hangman"
 var triesLeft; // tries remaining
 var guess;  // an array that shows the correct guesses, e.g.
 // ["h", "_", "n", "_", "_", "a", "n"]
-var lettersGuessed; // string of letters guessed
+var lettersGuessed; // array of chars (can be sorted)
 var score;
-var consecGuesses;
 
-// score system: +1 for each correct guess, bonus +1 for each consecutive correct guess
-// -1 for each mistake, resets consecutive streak
+// score system:
+// easy: +1 per correct, medium: +2, hard: +3
+// -1 per incorrect on all difficulties
 
 function initHangman(len) {
   // uses an API to get a random word of length `len`, and then stores that in `word`
@@ -24,8 +24,8 @@ function initHangman(len) {
   xhttp.onload = function () {
     if (this.status == 200) {
       word = JSON.parse(this.responseText)[0];
-      triesLeft = 8; // chose arbitrary value
-      lettersGuessed = "";
+      triesLeft = 9 - difficulty;
+      lettersGuessed = [];
       guess = new Array();
       isPlaying = true;
       score = 0;
@@ -41,11 +41,7 @@ function sendClue(header) {
   // bot sends a message for this clue
   var text = guess.join(' ') + "<br><br>Tries remaining: " + triesLeft;
   if (lettersGuessed) {
-    text += "<br><br><br>";
-    for (let i = 0; i < lettersGuessed.length; i++) {
-      if (i > 0) text += ' ';
-      text += lettersGuessed[i];
-    }
+    text += "<br><br><br>" + lettersGuessed.join(" ");
   }
   text += "<br><br>Score: " + score;
   addMessage(text, false, true);
@@ -56,7 +52,8 @@ function guessLetter(letter) {
   letter = letter.toUpperCase();
   if (!lettersGuessed.includes(letter)) {
     // has not yet been guessed
-    lettersGuessed += letter;
+    lettersGuessed[lettersGuessed.length] = letter;
+    lettersGuessed.sort();
     let foundMatch = false;
     for (let i = 0; i < word.length; i++) {
       if (word[i].toUpperCase() == letter) {
@@ -65,21 +62,20 @@ function guessLetter(letter) {
       }
     }
     if (foundMatch) {
-      addMessage("Found match for '" + letter + "'", false);
-      score += 1 + consecGuesses++;
+      addMessage("Found match for <b>" + letter + "</b>", false, true);
+      score += difficulty;
     } else {
-      addMessage("No match for '" + letter + "'", false);
-      consecGuesses = 0;
-      if (--score == -1) score = 0;
+      addMessage("No match for <b>" + letter + "</b>", false, true);
+      if (score-- == 0) score = 0;
     }
 
     if (!foundMatch && --triesLeft == 0) {
       sendClue();
-      addMessage('You lose.<br>The answer is: ' + word + '<br><br>Score: ' + score, false, true);
+      addMessage('You lose...<br><br>The answer is: <b>' + word + '</b><br><br>Score: ' + score, false, true);
       isPlaying = false;
     } else {
       sendClue();
-      if (guess.indexOf('_') == -1) {
+      if (!guess.includes('_')) {
         let highscore = highscores[difficulty - 1];
         let message = 'You win!<br><br>Score: ' + score + '<br>';
         if (score > highscore) {
@@ -101,7 +97,7 @@ function guessLetter(letter) {
 
 
   } else {
-    addMessage("'" + letter + "' has already been guessed.");
+    addMessage("<b>" + letter + "</b> has already been guessed.", false, true);
   }
 }
 
